572f2de9aca5b6975392d9d705e8e123c3f24011
maggie-5miles <maggie@wespoke.com>
2018-10-24 17:18:08 +0800

    manifest etc.

--- notices/notices.js
+++ notices/notices.js
@@ -17,14 +17,6 @@ module.exports = [
     body: fs.readFileSync(__dirname + '/archive/notice_2.md', 'utf8'),
   },
   {
-    id: 3,
-    read: false,
-    date: '',
-    title: 'Seed Phrase Alert',
-    firstVersion: '<=3.12.0',
-    body: fs.readFileSync(__dirname + '/archive/notice_3.md', 'utf8'),
-  },
-  {
     id: 4,
     read: false,
     date: '',
